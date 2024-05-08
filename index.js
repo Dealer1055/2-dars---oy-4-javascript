97f6ebfa93d9347b688e05038451610afc66f071
Azizbek Nosirov <abb923609@gmail.com>
2024-05-08 10:42:52 +0500

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+let matn = String(prompt("String turidagi ma'lumot kiriting "));
+let son = Number(prompt("Number turidagi ma'lumot kiriting "));
+let rost = Boolean(prompt("Boolean turidagi ma'lumot kiriting "));
+
+confirm("Hamma ma'lumotlar to'g'rimi");
+
+alert("Sizning kiritgan " + matn + " so'zi " + typeof matn + " typega tegishl? \n" +
+      "Sizning kiritgan " + son + " soni " + typeof son + " typega tegishli \n" +
+      "Sizning kiritgan " + rost + " so'zi " + typeof rost + " typega tegishli \n")
